f2f3d4a667f1429dd62b4c503ae5cd7f30191f84
nyaongnyaooong <luckyyou123@naver.com>
2023-04-13 17:28:13 +0900

    testCommit

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,9 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  console.log('test');
   res.render('index.html');
+  
 });
 
 module.exports = router;
